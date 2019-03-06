65116e8cafd9c669231a279cefbe1e3e17d6a9fa
Anton Ukhanev <xedin.unknown@gmail.com>
2019-03-06 11:05:37 +0100

    Added functionality to trash shortcodes from list page
    - Implemented generic handling for UI actions on backend.
    - Centralized some fixed values in class constants.
    - Added the trash action.

--- classes/class-handler-user-shortcodes-ui.php
+++ classes/class-handler-user-shortcodes-ui.php
@@ -2,19 +2,59 @@
 
 use GingerSoul\SoulCodes\Handler;
 use GingerSoul\SoulCodes\Query_Builder_Interface;
-use GingerSoul\SoulCodes\Template_Interface;
 
 /**
  * Handler class.
+ *
+ * @since [*next-version*]
  *
  * @package SoulCodes
  */
 class Handler_User_Shortcodes_Ui extends Handler {
 
+    const PARAM_ACTION = 'action';
+    const PARAM_NONCE = 'nonce';
+    const PARAM_ID = 'id';
+    const ACTION_TRASH_SHORTCODE = 'trash_shortcode';
+
 	/**
+     * {@inheritdoc}
+     *
 	 * @since [*next-version*]
 	 */
 	protected function hook() {
+	    add_action(
+	        'admin_init',
+            function () {
+	            global $pagenow;
+
+	            // Only for our page
+	            if (!$pagenow === 'admin.php') {
+	                return;
+                }
+
+                $string_filter_options = FILTER_FLAG_STRIP_BACKTICK
+                    | FILTER_FLAG_STRIP_HIGH
+                    | FILTER_FLAG_STRIP_LOW;
+                $page = filter_input(
+                    INPUT_GET,
+                    'page',
+                    FILTER_SANITIZE_STRING,
+                    $string_filter_options
+                );
+                $action = filter_input(
+                    INPUT_GET,
+                    self::PARAM_ACTION,
+                    FILTER_SANITIZE_STRING,
+                    $string_filter_options
+                );
+
+                if (!is_null($action)) {
+                    $this->handle_action($page, $action);
+                }
+            }
+        );
+
 		add_action(
 			'admin_menu',
 			function () {
@@ -23,6 +63,85 @@ class Handler_User_Shortcodes_Ui extends Handler {
 		);
 	}
 
+    /**
+     * Handles an action of a page.
+     *
+     * Useful for things like CRUD operations.
+     * Requires a nonce to be passed as part of the URL.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $page The key of the page to handle.
+     * @param string $action The key of the action to handle.
+     */
+	protected function handle_action($page, $action)
+    {
+        $nonce = filter_input(
+            INPUT_GET,
+            static::PARAM_NONCE,
+            FILTER_SANITIZE_STRING,
+            FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_BACKTICK
+            );
+
+        // Require valid nonce.
+        if (!$this->validate_nonce($nonce, $action)) {
+            wp_die(
+                wpautop($this->__('You are not authorized to perform this action.
+Possibly the page which dispatched the action has timed out.
+Please go back, refresh the page, and try again.')),
+                $this->__('Action Interrupted'),
+                [
+                    'back_link' => true,
+                ]);
+        }
+
+        switch ($page) {
+            case $this->get_config('user_shortcodes_list_page_name'):
+                $this->handle_list_page_action($action);
+
+                break;
+        }
+    }
+
+    /**
+     * Handles an action of the list page.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $action The key of the list page action to handle.
+     */
+    protected function handle_list_page_action($action) {
+        switch ($action) {
+            case static::ACTION_TRASH_SHORTCODE:
+                $id = abs(filter_input(
+                    INPUT_GET,
+                    static::PARAM_ID,
+                    FILTER_SANITIZE_NUMBER_INT
+                ));
+                $this->delete_shortcode($id);
+
+                break;
+        }
+    }
+
+    /**
+     * Deletes a shortcode by ID.
+     *
+     * @since [*next-version*]
+     *
+     * @param int $id The ID of the shortcode to delete.
+     *
+     * @return bool True if the post was deleted successfully; false otherwise.
+     */
+    protected function delete_shortcode($id) {
+	    return (bool) wp_delete_post($id);
+    }
+
+    /**
+     * Registers admin menus and their pages.
+     *
+     * @since [*next-version*]
+     */
 	protected function register_pages()
     {
         $menu_slug = $this->get_config('user_shortcodes_list_page_name');
@@ -38,32 +157,81 @@ class Handler_User_Shortcodes_Ui extends Handler {
         );
     }
 
+    /**
+     * Retrieves the output of the list page.
+     *
+     * @since [*next-version*]
+     *
+     * @return string Something that can be cast to string, which represents page output.
+     */
     protected function get_list_page_output()
     {
         $query = $this->get_shortcodes()->get();
         $total = $query->found_posts;
         $count = $query->post_count;
         $shortcodes = $query->get_posts();
+        $action_trash = static::ACTION_TRASH_SHORTCODE;
+        $trash_url_template = add_query_arg([
+            self::PARAM_ACTION        => $action_trash,
+            self::PARAM_NONCE         => '%1$s',
+            self::PARAM_ID            => '%2$s',
+        ]);
+        $nonce = $this->create_nonce($action_trash);
 
         $list = $this->create_template_block($this->get_template('user_shortcodes_list'), [
             'shortcodes'            => $shortcodes,
             'total'                 => $total,
             'count'                 => $count,
+            'nonce'                 => $nonce,
+            'trash_url_template'    => $trash_url_template,
         ]);
         $page = $this->create_template_block($this->get_template('user_shortcodes_list_page'), [
             'list'          => $list,
         ]);
 
+
         return $page;
     }
 
     /**
-     * @return Query_Builder_Interface
+     * Retrieves a query builder for shortcode queries.
+     *
+     * @since [*next-version*]
+     *
+     * @return Query_Builder_Interface The shortcode query builder.
      */
     protected function get_shortcodes() {
 	    $builder = $this->get_config('user_shortcodes_query_builder');
         assert($builder instanceof Query_Builder_Interface);
 
         return $builder;
+    }
+
+    /**
+     * Creates a nonce for the specified key.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $key The key to create the nonce for.
+     *
+     * @return string A string or stringable object that represents a nonce.
+     */
+    protected function create_nonce($key) {
+        return wp_create_nonce($key);
+    }
+
+    /**
+     * Determines whether a nonce is valid.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $nonce A string or stringable value that represents a nonce to validate.
+     * @param string $key The key to validate the nonce for.
+     *
+     * @return false|int A positive integer if the nonce is valid. False otheriwse.
+     * See {@see wp_verify_nonce()}.
+     */
+    protected function validate_nonce($nonce, $key) {
+        return wp_verify_nonce($nonce, $key);
     }
 }
--- templates/user_shortcodes_list.php
+++ templates/user_shortcodes_list.php
@@ -7,6 +7,8 @@ $shortcodes = $c('shortcodes');
 $total = $c('total');
 $count = $c('count');
 $strings = $c('strings');
+$nonce = $c('nonce');
+$trash_url_template = $c('trash_url_template');
 $summary = $t('Showing %1$d of %2$d records', [$count, $total]);
 ?>
 
@@ -22,6 +24,7 @@ $summary = $t('Showing %1$d of %2$d records', [$count, $total]);
                 <tr>
                     <th class="shortcode-shortcode"><?= esc_html($t('Shortcode')) ?></th>
                     <th class="shortcode-template"><?= esc_html($t('Template')) ?></th>
+                    <th class="shortcode-actions"><?= esc_html($t('Actions')) ?></th>
                 </tr>
             </thead>
             <tbody>
@@ -29,6 +32,9 @@ $summary = $t('Showing %1$d of %2$d records', [$count, $total]);
                 <tr>
                     <td class="shortcode-shortcode"><?= esc_html(sprintf('[%1$s]', $shortcode->post_name)) ?></td>
                     <td class="shortcode-template"><?= esc_html($shortcode->post_content) ?></td>
+                    <td class="shortcode-actions">
+                        <a href="<?= esc_attr(vsprintf($trash_url_template, [$nonce, $shortcode->ID])) ?>" class="action action-trash"><?= esc_html($t('Trash', [], 'The trash action')) ?></a>
+                    </td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
