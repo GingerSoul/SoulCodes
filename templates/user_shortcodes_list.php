63a8ef4ffc50a8cc44bf8debc46e32c0be26d104
Anton Ukhanev <xedin.unknown@gmail.com>
2019-03-06 15:39:09 +0100

    Fixed bug with shortcode saving
    This bug caused only the last shortcode in the list to be saved.

--- templates/user_shortcodes_list.php
+++ templates/user_shortcodes_list.php
@@ -25,36 +25,37 @@ $summary = $t('Showing %1$d of %2$d records', [$count, $total]);
         </div>
     </div>
     <div class="list-body">
-        <form action="<?= esc_attr(vsprintf($save_url_template, [$nonce])) ?>" method="post">
-            <table>
-                <thead>
-                    <tr>
-                        <th class="shortcode-shortcode"><?= esc_html($t('Shortcode')) ?></th>
-                        <th class="shortcode-template"><?= esc_html($t('Template')) ?></th>
-                        <th class="shortcode-actions"><?= esc_html($t('Actions')) ?></th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($shortcodes as $shortcode): ?>
-                    <tr>
-                        <td class="shortcode-shortcode">
-                            &lsqb;
-                            <input type="text" name="<?= esc_attr($params['name']) ?>" value="<?= esc_attr($shortcode->post_name) ?>" title="<?= esc_attr($t('Shortcode')) ?>" />
-                            &rsqb;
-                        </td>
-                        <td class="shortcode-template">
-                            <textarea name="<?= esc_attr($params['template']) ?>" title="<?= esc_attr($t('Template')) ?>"><?= esc_html($shortcode->post_content) ?></textarea>
-                        </td>
-                        <td class="shortcode-actions">
+        <table>
+            <thead>
+                <tr>
+                    <th class="shortcode-shortcode"><?= esc_html($t('Shortcode')) ?></th>
+                    <th class="shortcode-template"><?= esc_html($t('Template')) ?></th>
+                    <th class="shortcode-actions"><?= esc_html($t('Actions')) ?></th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($shortcodes as $shortcode): ?>
+                <?php $form_id = sprintf('shortcode_row_form_%1$s', $shortcode->ID) ?>
+                <tr>
+                    <td class="shortcode-shortcode">
+                        &lsqb;
+                        <input form="<?= $form_id ?>" type="text" name="<?= esc_attr($params['name']) ?>" value="<?= esc_attr($shortcode->post_name) ?>" title="<?= esc_attr($t('Shortcode')) ?>" />
+                        &rsqb;
+                    </td>
+                    <td class="shortcode-template">
+                        <textarea form="<?= $form_id ?>" name="<?= esc_attr($params['template']) ?>" title="<?= esc_attr($t('Template')) ?>"><?= esc_html($shortcode->post_content) ?></textarea>
+                    </td>
+                    <td class="shortcode-actions">
+                        <form action="<?= esc_attr(vsprintf($save_url_template, [$nonce])) ?>" method="post" id="<?= $form_id ?>">
                             <input type="hidden" name="<?= esc_attr($params['id']) ?>" value="<?= esc_attr($shortcode->ID) ?>" />
                             <a href="<?= esc_attr(vsprintf($trash_url_template, [$nonce, $shortcode->ID])) ?>" class="action action-trash"><?= esc_html($t('Trash', [], 'The trash action')) ?></a>
                             <input type="submit" value="<?= $t('Save') ?>" />
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-        </form>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
     <div class="list-foot">
         <div class="list-summary">
