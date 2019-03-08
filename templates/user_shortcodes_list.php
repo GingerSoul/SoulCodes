8283e5eda3c14204752edee5d466d77de44981dd
Anton Ukhanev <xedin.unknown@gmail.com>
2019-03-08 16:22:15 +0100

    Removed redundant commented HTML and fixed formatting

--- templates/user_shortcodes_list.php
+++ templates/user_shortcodes_list.php
@@ -24,103 +24,39 @@ $summary = $t('Showing %1$d of %2$d records', [$count, $total]);
             <?= esc_html($summary) ?>
         </div>
     </div>
+
     <div class="list-body">
-	    
         <?php foreach ($shortcodes as $shortcode): ?>
-     	   <?php $form_id = sprintf('shortcode_row_form_%1$s', $shortcode->ID) ?>
-    	
-	 	   <div class="shortcode-item">                
-                
+            <?php $form_id = sprintf('shortcode_row_form_%1$s', $shortcode->ID) ?>
+            <div class="shortcode-item">
                 <div class="shortcode-code">
-                        
-                        <input form="<?= $form_id ?>" type="text" name="<?= esc_attr($params['name']) ?>" value="<?= esc_attr($shortcode->post_name) ?>" title="<?= esc_attr($t('Shortcode')) ?>" />
-                        
-                        <button>Copy</button>
-	                
+                    <input form="<?= $form_id ?>" type="text" name="<?= esc_attr($params['name']) ?>"
+                           value="<?= esc_attr($shortcode->post_name) ?>" title="<?= esc_attr($t('Shortcode')) ?>"/>
+                    <button>Copy</button>
                 </div>
-                
                 <div class="shortcode-temp">
-                        
-                        <textarea form="<?= $form_id ?>" name="<?= esc_attr($params['template']) ?>" title="<?= esc_attr($t('Template')) ?>"><?= esc_html($shortcode->post_content) ?></textarea>
-	                
+                    <textarea
+                            form="<?= $form_id ?>" name="<?= esc_attr($params['template']) ?>"
+                            title="<?= esc_attr($t('Template')) ?>"
+                    ><?= esc_html($shortcode->post_content) ?></textarea>
                 </div>
-                
+
                 <div class="shortcode-act">
-                        
-                        <form action="<?= esc_attr(vsprintf($save_url_template, [$nonce])) ?>" method="post" id="<?= $form_id ?>">
-                            <input type="hidden" name="<?= esc_attr($params['id']) ?>" value="<?= esc_attr($shortcode->ID) ?>" />
-                            <a href="<?= esc_attr(vsprintf($trash_url_template, [$nonce, $shortcode->ID])) ?>" class="action action-trash"><?= esc_html($t('Trash', [], 'The trash action')) ?></a>
-                            <input type="submit" value="<?= $t('Save') ?>" />
-                        </form>
-	                
+                    <form action="<?= esc_attr(vsprintf($save_url_template, [$nonce])) ?>" method="post" id="<?= $form_id ?>">
+                        <input type="hidden" name="<?= esc_attr($params['id']) ?>" value="<?= esc_attr($shortcode->ID) ?>"/>
+                        <a
+                                href="<?= esc_attr(vsprintf($trash_url_template, [$nonce, $shortcode->ID])) ?>"
+                                class="action action-trash">
+                            <?= esc_html($t('Trash', [], 'The trash action')) ?>
+                        </a>
+                        <input type="submit" value="<?= $t('Save') ?>"/>
+                    </form>
                 </div>
+            </div>
+        <?php endforeach; ?>
 
-                
-	 	   </div>
-<!--
-                
-                <tr>
-                    <td class="shortcode-shortcode">
-                        &lsqb;
-                        <input form="<?= $form_id ?>" type="text" name="<?= esc_attr($params['name']) ?>" value="<?= esc_attr($shortcode->post_name) ?>" title="<?= esc_attr($t('Shortcode')) ?>" />
-                        &rsqb;
-                    </td>
-                    <td class="shortcode-template">
-                        <textarea form="<?= $form_id ?>" name="<?= esc_attr($params['template']) ?>" title="<?= esc_attr($t('Template')) ?>"><?= esc_html($shortcode->post_content) ?></textarea>
-                    </td>
-                    <td class="shortcode-actions">
-                        <form action="<?= esc_attr(vsprintf($save_url_template, [$nonce])) ?>" method="post" id="<?= $form_id ?>">
-                            <input type="hidden" name="<?= esc_attr($params['id']) ?>" value="<?= esc_attr($shortcode->ID) ?>" />
-                            <a href="<?= esc_attr(vsprintf($trash_url_template, [$nonce, $shortcode->ID])) ?>" class="action action-trash"><?= esc_html($t('Trash', [], 'The trash action')) ?></a>
-                            <input type="submit" value="<?= $t('Save') ?>" />
-                        </form>
-                    </td>
-                </tr>
--->
-
-            <?php endforeach; ?>
-	    	
-	    	
-    	</div>
-
-<!--
-    <div class="list-body">
-        <table>
-            <thead>
-                <tr>
-                    <th class="shortcode-shortcode"><?= esc_html($t('Shortcode')) ?></th>
-                    <th class="shortcode-template"><?= esc_html($t('Template')) ?></th>
-                    <th class="shortcode-actions"><?= esc_html($t('Actions')) ?></th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($shortcodes as $shortcode): ?>
-                <?php $form_id = sprintf('shortcode_row_form_%1$s', $shortcode->ID) ?>
-                <tr>
-                    <td class="shortcode-shortcode">
-                        &lsqb;
-                        <input form="<?= $form_id ?>" type="text" name="<?= esc_attr($params['name']) ?>" value="<?= esc_attr($shortcode->post_name) ?>" title="<?= esc_attr($t('Shortcode')) ?>" />
-                        &rsqb;
-                    </td>
-                    <td class="shortcode-template">
-                        <textarea form="<?= $form_id ?>" name="<?= esc_attr($params['template']) ?>" title="<?= esc_attr($t('Template')) ?>"><?= esc_html($shortcode->post_content) ?></textarea>
-                    </td>
-                    <td class="shortcode-actions">
-                        <form action="<?= esc_attr(vsprintf($save_url_template, [$nonce])) ?>" method="post" id="<?= $form_id ?>">
-                            <input type="hidden" name="<?= esc_attr($params['id']) ?>" value="<?= esc_attr($shortcode->ID) ?>" />
-                            <a href="<?= esc_attr(vsprintf($trash_url_template, [$nonce, $shortcode->ID])) ?>" class="action action-trash"><?= esc_html($t('Trash', [], 'The trash action')) ?></a>
-                            <input type="submit" value="<?= $t('Save') ?>" />
-                        </form>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
     </div>
--->
-    
-    
-    
+
     <div class="list-foot">
         <div class="list-summary">
             <?= esc_html($summary) ?>
